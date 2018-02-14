68f7b7451b7bdb0557cbe4cae5277633bca71838
floodd <floodd@wit.edu>
2018-02-13 21:46:10 -0500

    Not Updated yet

---
+++ UI/src/UpdateAccountController.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UpdateAccountController implements Initializable, ControlledScreen{
+
+    ScreensController myController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void setScreenParent(ScreensController screenParent){
+        myController = screenParent;
+    }
+
+    @FXML
+    private void goToHomePage(ActionEvent event){
+        myController.setScreen(Main.HomePageID);
+    }
+
+    @FXML
+    private void goToCrypto(ActionEvent event){
+        myController.setScreen(Main.CryptoCurrenciesID);
+    }
+
+    @FXML
+    private void goToViewTransactions(ActionEvent event){
+        myController.setScreen(Main.ViewTransactionsID);
+    }
+
+    @FXML
+    private void goToAddTransaction(ActionEvent event){
+        myController.setScreen(Main.AddTransactionID);
+    }
+
+    @FXML
+    private void goToLoginPage(ActionEvent event){
+        myController.setScreen(Main.LoginID);
+    }
+
+}
