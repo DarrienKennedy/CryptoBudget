406ea640f9549d1a89facbc27947083d2316c674
floodd <floodd@wit.edu>
2018-02-22 11:02:42 -0500

    Update UX

---
+++ src/sample/AddTransactionController.java
@@ -0,0 +1,58 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddTransactionController implements Initializable, ControlledScreen{
+
+    ScreensController myController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void setScreenParent(ScreensController screenParent){
+        myController = screenParent;
+    }
+
+    @FXML
+    private void goToHomePage(ActionEvent event){
+        myController.setScreen(Main.HomePageID);
+    }
+
+    @FXML
+    private void goToCrypto(ActionEvent event){
+        myController.setScreen(Main.CryptoCurrenciesID);
+    }
+
+    @FXML
+    private void goToUpdateAccount(ActionEvent event){
+        myController.setScreen(Main.UpdateAccountID);
+    }
+
+    @FXML
+    private void goToViewTransactions(ActionEvent event){
+        myController.setScreen(Main.ViewTransactionsID);
+    }
+
+    @FXML
+    private void goToLoginPage(ActionEvent event){
+        myController.setScreen(Main.LoginID);
+    }
+
+    @FXML
+    private void goToEditGoalsPage(ActionEvent event){ myController.setScreen(EditGoals.LoginID);
+    }
+
+
+
+
+
+
+
+}
