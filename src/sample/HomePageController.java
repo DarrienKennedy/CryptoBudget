6c30de2b7e34cfb2a4cfc50b359bbdb4d5792280
floodd <floodd@wit.edu>
2018-02-24 15:14:23 -0500

    Fixed some Incorrect Naming

---
+++ src/sample/HomePageController.java
@@ -0,0 +1,56 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomePageController implements Initializable, ControlledScreen{
+
+    ScreensController myController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void setScreenParent(ScreensController screenParent){
+        myController = screenParent;
+    }
+
+    @FXML
+    private void goToCrypto(ActionEvent event){
+        myController.setScreen(Main.CryptoCurrenciesID);
+    }
+
+    @FXML
+    private void goToUpdateAccount(ActionEvent event){
+        myController.setScreen(Main.UpdateAccountID);
+    }
+
+    @FXML
+    private void goToViewTransactions(ActionEvent event){
+        myController.setScreen(Main.ViewTransactionsID);
+    }
+
+    @FXML
+    private void goToAddTransaction(ActionEvent event){
+        myController.setScreen(Main.AddTransactionID);
+    }
+
+    @FXML
+    private void goToLoginPage(ActionEvent event){
+        myController.setScreen(Main.LoginID);
+    }
+
+    @FXML
+    private void goToEditGoalsPage(ActionEvent event){ myController.setScreen(Main.EditGoalsID);
+    }
+
+
+
+
+}
